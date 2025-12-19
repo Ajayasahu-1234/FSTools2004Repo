8ead8ae3aba261b94d97fcbd297b735bf84a0cc0
Dell <Dell@DESKTOP-HFVU18M>
2025-12-19 13:50:53 +0530

    changes done by TL

--- GITProj01/src/main/java/com/nit/main/Arithmatic.java
+++ GITProj01/src/main/java/com/nit/main/Arithmatic.java
@@ -1,12 +1,14 @@
 package com.nit.main;
 
 public class Arithmatic {
-     
+
 	public  double sum(double x,double y) {
 		return x+y;
 	}
+  
 	
 	public double sub(double x, double y) {
 		return x-y;
+
 	}
 }
